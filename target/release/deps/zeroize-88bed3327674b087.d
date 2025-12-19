1e6e4c7cbbe3f717a620892db2404e3f9120832e
chrischeng-c4 <chris.cheng.c4@gmail.com>
2025-12-19 19:06:26 +0800

    chore: remove target/ from git tracking
    - target/ should only be in .gitignore, not tracked by git
    - Cleaned up all previously tracked build artifacts

--- target/release/deps/zeroize-88bed3327674b087.d
+++
@@ -1,8 +0,0 @@
-/Users/chris.cheng/chris-project/data-bridge/target/release/deps/zeroize-88bed3327674b087.d: /Users/chris.cheng/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zeroize-1.8.2/src/lib.rs /Users/chris.cheng/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zeroize-1.8.2/src/aarch64.rs
-
-/Users/chris.cheng/chris-project/data-bridge/target/release/deps/libzeroize-88bed3327674b087.rlib: /Users/chris.cheng/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zeroize-1.8.2/src/lib.rs /Users/chris.cheng/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zeroize-1.8.2/src/aarch64.rs
-
-/Users/chris.cheng/chris-project/data-bridge/target/release/deps/libzeroize-88bed3327674b087.rmeta: /Users/chris.cheng/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zeroize-1.8.2/src/lib.rs /Users/chris.cheng/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zeroize-1.8.2/src/aarch64.rs
-
-/Users/chris.cheng/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zeroize-1.8.2/src/lib.rs:
-/Users/chris.cheng/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zeroize-1.8.2/src/aarch64.rs:
